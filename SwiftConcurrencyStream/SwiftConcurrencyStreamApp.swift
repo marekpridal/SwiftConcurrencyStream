6cb97047ce8c274c91f545986f124bb0b30b3b5f
Marek Pridal <marek@marekpridal.eu>
2025-03-17 09:45:20 +0100

    Initial Commit

---
+++ SwiftConcurrencyStream/SwiftConcurrencyStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyStreamApp.swift
+//  SwiftConcurrencyStream
+//
+//  Created by Marek Pridal on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
